e3db49c1af28b88eae3544f27e2de756d0d71371
Elivelton Gustavo <elivelton.gustavo34@gmail.com>
2020-02-28 00:36:22 -0300

    first commit

---
+++ app/src/main/java/br/com/etelimoeiro/projetointegrador/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.etelimoeiro.projetointegrador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
